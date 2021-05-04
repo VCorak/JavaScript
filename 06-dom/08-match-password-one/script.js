380d534fe597fa1ee57afb03337776f92fb840d3
Valentina Corak <valentinacorak@gmail.com>
2021-05-04 10:28:26 +0200

    Solution 06/08

--- 06-dom/08-match-password-one/script.js
+++ 06-dom/08-match-password-one/script.js
@@ -11,6 +11,19 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        let passOne = document.getElementById("pass-one");
+        let passTwo = document.getElementById("pass-two");
+        if (passOne.value !== passTwo.value) {
+            passOne.style.backgroundColor = "red";
+            passTwo.style.backgroundColor = "red";
+
+        }
+        else {
+            passOne.style.backgroundColor = "white";
+            passTwo.style.backgroundColor = "white";
+        }
+    });
+
 
 })();
