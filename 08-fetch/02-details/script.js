502926b8cdd044ee1f91b4c6a1ba15dfe5141a97
Valentina Corak <valentinacorak@gmail.com>
2021-05-05 16:46:52 +0200

    Finaaaly messy solution to 08/02

--- 08-fetch/02-details/script.js
+++ 08-fetch/02-details/script.js
@@ -14,16 +14,23 @@
     function getInputValue() {
 
         document.getElementById('run').addEventListener('click', () => {
-            let input = document.getElementById("hero-id").value;
-
-            fetch('http://localhost:8000/_shared/api.json',{ input : id})
+        let input = +document.getElementById("hero-id").value;
+            fetch('http://localhost:8000/_shared/api.json')
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
-                    console.log(data.heroes);
                     const list = data.heroes.map(heroes => {
-                        return `<li>Name: ${heroes.name}</li>`
+                        console.log(input, heroes.id);
+                    if (input === heroes.id) {
+                        return `
+                            Id: ${heroes.id}
+                            Name: ${heroes.name}
+                            Alterego: ${heroes.alterEgo}
+                            Abilities: ${heroes.abilities}
+                        `
+                    }
+
                     })
                     document.querySelector('#target')
                         .insertAdjacentHTML("afterbegin", list);
@@ -31,6 +38,21 @@
         })
     }
     getInputValue();
+
+   /*const api_url = 'http://localhost:8000/_shared/api.json';
+    document.getElementById('run').addEventListener('click', () => {
+        async function getHeroes() {
+            const response = await fetch(api_url);
+            const data = await response.json();
+            const { heroes } = data;
+            document.querySelector('#target')
+                .innerHTML(heroes);
+        }
+        getHeroes();
+    }) */
+
+
+
     // When you click on the button,
     // get the id from the form, then
     // get the corresponding X-Men from
